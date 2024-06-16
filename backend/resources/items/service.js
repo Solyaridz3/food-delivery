ca292f05ec76a5bbf57a6cc15058f838ad7b8d88
solyaridz3 <solyaridz3@gmail.com>
2024-06-16 14:00:21 +0300

    README update

--- backend/resources/items/service.js
+++ backend/resources/items/service.js
@@ -1,8 +1,11 @@
 import pool from "../../db.js";
 
-class ItemsService {
-    create = async () => {
-        await pool.query();
+class ItemService {
+    create = async (name, price, description, ingredients, image) => {
+        await pool.query(
+            "INSERT INTO items (name, price, description, ingredients, image) VALUES ($1, $2, $3, $4, $5)",
+            [name, price, description, ingredients, image]
+        );
     };
 
     change = async () => {};
