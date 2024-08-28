d325f0df473113cec34a204885a5975635c1fcde
solyaridz3 <solyaridz3@gmail.com>
2024-08-28 14:01:58 +0300

    users not nullable values fix and user phone added

--- backend/resources/user/queries.js
+++ backend/resources/user/queries.js
@@ -1,8 +1,8 @@
 const setup =
-    "CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(100), email VARCHAR(100) UNIQUE, password CHAR(60), userRole VARCHAR(30))";
+    "CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, password CHAR(60) NOT NULL, phone VARCHAR(20) UNIQUE, user_role VARCHAR(30))";
 
 const register =
-    "INSERT INTO users (name, email, password, userRole) VALUES ($1, $2, $3, $4)";
+    "INSERT INTO users (name, email, password, user_role) VALUES ($1, $2, $3, $4)";
 
 const getByEmail = "SELECT * FROM users WHERE email = $1";
 
@@ -10,7 +10,8 @@ const getById = "SELECT * FROM users WHERE id = $1";
 
 const getAll = "SELECT * FROM users";
 
-const updateUser = "UPDATE users SET name = $2, email = $3, password = $4 WHERE id = $1";
+const updateUser =
+    "UPDATE users SET name = $2, email = $3, password = $4 WHERE id = $1";
 
 const updateName = "SET name = $1 WHERE id = $2";
 
